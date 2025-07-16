92c87debc78effe5df175b378ed835223f19da14
Shuan-Nazar <jmandras@optonline.net>
2025-07-16 19:26:12 -0400

    adding cursor circle

---
+++ cursor.js
@@ -0,0 +1,33 @@
+const $bigBall = document.querySelector('.cursor__ball--big');
+const $hoverables = document.querySelectorAll('a, .link');
+
+// Listeners
+document.body.addEventListener('mousemove', onMouseMove);
+for (let i = 0; i < $hoverables.length; i++)
+{
+    $hoverables[i].addEventListener('mouseenter', onMouseHover);
+    $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+}
+
+// Move the cursor
+function onMouseMove(e)
+{
+    TweenMax.to($bigBall, 0.15, {
+        x: e.pageX - 8,
+        y: e.pageY - 8,
+    })
+}
+
+// Hover an element
+function onMouseHover()
+{
+    TweenMax.to($bigBall, .3, {
+        scale: 3
+    })
+}
+function onMouseHoverOut()
+{
+    TweenMax.to($bigBall, .1, {
+        scale: 1
+    })
+}
