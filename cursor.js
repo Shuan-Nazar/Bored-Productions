11dd951742209b4e523d439babca5d3d9b172f61
Shuan-Nazar <jmandras@optonline.net>
2025-07-16 20:16:56 -0400

    fixing circle

--- cursor.js
+++ cursor.js
@@ -1,22 +1,36 @@
-const circleElement = document.querySelector('.cursor');
-
-const mouse = { x:0, y:0 },
-      circle = { x:0, y:0 }
-
-window.addEventListener('mousemove', e => {
-    mouse.x = e.x;
-    mouse.y = e.y;
-});
-
-const speed = 0.15;
-
-const tick = () => {
-    circle.x += (mouse.x - circle.x) * speed;
-    circle.y += (mouse.y - circle.y) * speed;
-
-    circleElement.style.transform = `translate(${circle.x}px, ${circle.y}px)`;
-
-    window.requestAnimationFrame(tick);
-}
-
-tick();
+(function() {
+    console.clear();
+    
+    const follower = document.querySelector('.cursor');
+    
+    let posX = 0;
+    let posY = 0;
+    let mouseX = 0;
+    let mouseY = 0;
+    
+    const ease = 0.1;
+    
+    function easeTo() {    
+      const followerBounds = follower.getBoundingClientRect();
+      
+      const dX = mouseX - (followerBounds.left + 16);
+      const dY = mouseY - (followerBounds.top + 16);
+      
+      posX += dX * ease;
+      posY += dY * ease;
+    }
+    
+    function update() {
+      easeTo();
+      follower.style.transform = `translate3d(${posX}px, ${posY}px, 0)`;  
+      requestAnimationFrame(update);
+    }
+    
+    function setCoords(e) {
+      mouseX = e.clientX;
+      mouseY = e.clientY;
+    }
+    
+      document.onmousemove = setCoords;
+    update();
+  })();
