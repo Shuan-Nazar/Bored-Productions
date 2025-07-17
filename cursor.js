c732b397db0cd8d66c941427aee93c6d8854d75c
Shuan-Nazar <jmandras@optonline.net>
2025-07-16 20:46:04 -0400

    fixing circle

--- cursor.js
+++ cursor.js
@@ -1,4 +1,4 @@
-(function() {
+/*(function() {
     console.clear();
     
     const follower = document.querySelector('.cursor');
@@ -33,4 +33,35 @@
     
       document.onmousemove = setCoords;
     update();
-  })();
+  })();*/
+
+
+const $bigBall = document.querySelector('.cursor');
+const $hoverables = document.querySelectorAll('a, .link');
+
+// Listeners
+document.body.addEventListener('mousemove', onMouseMove);
+for (let i = 0; i < $hoverables.length; i++) {
+    $hoverables[i].addEventListener('mouseenter', onMouseHover);
+    $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+}
+
+// Move the cursor
+function onMouseMove(e) {
+    TweenMax.to($bigBall, 0.15, {
+        x: e.pageX - 8,
+        y: e.pageY - 8,
+    })
+}
+
+// Hover an element
+function onMouseHover() {
+    TweenMax.to($bigBall, .3, {
+        scale: 3
+    })
+}
+function onMouseHoverOut() {
+    TweenMax.to($bigBall, .1, {
+        scale: 1
+    })
+}  
