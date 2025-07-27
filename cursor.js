b3a7113da4918ee7307d54fa830a2e21f89f82d5
Shuan-Nazar <jmandras@optonline.net>
2025-07-27 16:09:16 -0400

    circle shit

--- cursor.js
+++ cursor.js
@@ -1,66 +1,17 @@
-/*const circle = document.querySelector('.cursor');
-const $hoverables = document.querySelectorAll('a');
+const circle = document.getElementById('.cursor')
+const circleStyle = circle.style
+const hoverables = document.getElementById('a')
 
-// Listeners
-document.body.addEventListener('mousemove', onMouseMove);
-for (let i = 0; i < $hoverables.length; i++) {
-    $hoverables[i].addEventListener('mouseenter', onMouseHover);
-    $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+document.addEventListener('mousemove', e => {
+  circleStyle.top = `${e.clientY - circle.offsetHeight/2}px`
+  circleStyle.left = `${e.clientX - circle.offsetWidth/2}px`
+})
+
+for(elm in hoverables) {
+  elm.addEventListener('mouseenter', e => {
+    circleStyle.scale = e.scale * 3
+  })
+  elm.addEventListener('mouseleave', e => {
+    circleStyle.scale = e.scale/3
+  })
 }
-
-// Move the cursor
-function onMouseMove(e) {
-    TweenMax.to($bigBall, 0.15, {
-        x: e.pageX - 8,
-        y: e.pageY - 8,
-    })
-}
-
-// Hover an element
-function onMouseHover() {
-    TweenMax.to($bigBall, .3, {
-        scale: 3
-    })
-}
-function onMouseHoverOut() {
-    TweenMax.to($bigBall, .1, {
-        scale: 1
-    })
-}*/
-
-(function() {
-    console.clear();
-    
-    const follower = document.querySelector('.cursor');
-    
-    let posX = 0;
-    let posY = 0;
-    let mouseX = 0;
-    let mouseY = 0;
-    
-    const ease = 0.1;
-    
-    function easeTo() {    
-      const followerBounds = follower.getBoundingClientRect();
-      
-      const dX = mouseX - (followerBounds.left + 16);
-      const dY = mouseY - (followerBounds.top + 16);
-      
-      posX += dX * ease;
-      posY += dY * ease;
-    }
-    
-    function update() {
-      easeTo();
-      follower.style.transform = `translate3d(${posX}px, ${posY}px, 0)`;  
-      requestAnimationFrame(update);
-    }
-    
-    function setCoords(e) {
-      mouseX = e.clientX;
-      mouseY = e.clientY;
-    }
-    
-      document.onmousemove = setCoords;
-    update();
-  })();
