42ca2251b6b71adf08d05eb311443ca54ffeeafe
Shuan-Nazar <jmandras@optonline.net>
2025-07-27 16:41:42 -0400

    circle shit

--- cursor.js
+++ cursor.js
@@ -1,12 +1,12 @@
 const circle = document.querySelector('.cursor')
-const hoverables = document.querySelectorAll('button', 'a')
+const hoverables = document.querySelectorAll('button, a')
 
 document.addEventListener('mousemove', e => {
   circle.style.top = `${e.clientY - circle.offsetHeight/6}px`
   circle.style.left = `${e.clientX - circle.offsetWidth/3}px`
 })
 
-for(elm in hoverables) {
+for(elm of hoverables) {
   elm.addEventListener('mouseenter', () => {
     circle.style.scale = 3
   })
